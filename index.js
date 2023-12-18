c9cf292f79a350423ac88f3b948cb1bec7f44613
Bhola Kumar <87958764+Bhola-kumar@users.noreply.github.com>
2023-12-18 16:47:07 +0530

    update3.0
    organised the code and introduced completely new UI with additional feature of copy code , reset button.

--- index.js
+++ index.js
@@ -1,109 +1,99 @@
-import { bubbleSort } from './algorithms/bubbleSort.js';
-import { selectionSort } from './algorithms/selectionSort.js';
-import { insertionSort } from './algorithms/insertionSort.js';
-import { quickSort } from './algorithms/quickSort.js';
-import { mergeSort } from './algorithms/mergeSort.js';
-import { heapSort } from './algorithms/heapSort.js';
-import { generateRandomArray } from './input.js';
-import { animateSoundBars } from './input.js';
-import { createBars } from './input.js';
-import { bubbleSortCode } from './algorithms/allSortingMethods.js';
-import { selectionSortCode } from "./algorithms/allSortingMethods.js";
-import { insertionSortCode } from "./algorithms/allSortingMethods.js";
-import { quickSortCode } from "./algorithms/allSortingMethods.js";
-import { mergeSortCode } from "./algorithms/allSortingMethods.js";
-import { heapSortCode } from "./algorithms/allSortingMethods.js";
+import { bubbleSortCode, bubbleStart } from './algorithms/bubbleSort.js';
+import { selectionSortCode, selectionStart } from './algorithms/selectionSort.js';
+import { insertionSortCode, insertionStart } from './algorithms/insertionSort.js';
+import { quickSortCode, quickStart } from './algorithms/quickSort.js';
+import { mergeSortCode, mergeStart } from './algorithms/mergeSort.js';
+import { heapSortCode, heapStart } from './algorithms/heapSort.js';
+import { generateRandomArray,animateBars,createBars,showToast,copyCode} from './input.js';
 
-document.addEventListener('DOMContentLoaded',  () => {
-  let arrSize=15;
-  let sliderForSize=document.getElementById('arraySize');
-  let sizeValue=document.getElementById('arraySizeValue');
-  function getarraySize(){
-    let ans=sliderForSize.value;
-    arrSize=ans;
-    sizeValue.textContent=arrSize;
-  }
-  sliderForSize.addEventListener("input", getarraySize);
+document.addEventListener('DOMContentLoaded', () => {
 
-  let mini=1,maxi=101;
-  let gen = document.getElementsByClassName('btn')[0];
-  let arr=generateRandomArray(arrSize,mini,maxi);
+  // (on loading the page)
+  let arrSize = 25;
+  let arr = generateRandomArray(arrSize);
   createBars(arr);
-  animateSoundBars();
-  let generateArrayCheck=false;
-  gen.addEventListener('click',()=>{
-    arr=generateRandomArray(arrSize,mini,maxi);
+  animateBars();
+
+  //controller - 1 (on changing the slider value)
+  let sliderValue = document.getElementById('slider');
+  let visibleValue = document.getElementById('valueonslider');
+  sliderValue.addEventListener("input", () => { arrSize = sliderValue.value; visibleValue.textContent = sliderValue.value; });
+
+  //controller - 2 (on clicking the Create button)
+  let generateArrayCheck = false;
+  let gen = document.getElementById('gen');
+  gen.addEventListener('click', () => {
+    arr = generateRandomArray(arrSize);
     createBars(arr);
-    generateArrayCheck=true;
+    generateArrayCheck = true;
   });
 
-  
-  let btn = document.getElementsByClassName('btn')[1];
+  //controller - 3 (on clicking the Start button)
+  let btn = document.getElementById('start');
   btn.addEventListener('click', () => {
-    if(generateArrayCheck){
-    let selectedOption = document.querySelector('.options');
-    let sortingMethod = selectedOption.value;
-    let selectedLanguage = document.querySelector(".language-option");
-    selectedLanguage.selectedIndex = 1; 
-    switch (sortingMethod) {
-      case "bubbleSort":
-        bubbleSort(arr);
-        bubbleSortCode();
-        break;
-      case "selectionSort":   
-        selectionSort(arr);
-        selectionSortCode();
-        break;
-      case "insertionSort":
-        insertionSort(arr);
-        insertionSortCode();
-        break;
-      case "quickSort":
-        let speed=50;
-        quickSort(arr,0,arr.length-1,speed);
-        quickSortCode();
-        break;
-      case "mergeSort":
-        mergeSort(arr,0,arr.length-1);
-        mergeSortCode();
-        break;
-      case "heapSort":
-        heapSort(arr,arr.length);
-        heapSortCode();
-        break;
-      default:
+    if (generateArrayCheck) {
+      let sortingMethod = document.querySelector('.options').value;
+      let selectedLanguage = document.querySelector(".language-option");
+      if (selectedLanguage.selectedIndex === 0) selectedLanguage.selectedIndex = 1;
+      generateArrayCheck = false;
+      if (sortingMethod == "bubbleSort") bubbleStart(arr);
+      else if (sortingMethod == "selectionSort") selectionStart(arr);
+      else if (sortingMethod == "insertionSort") insertionStart(arr);
+      else if (sortingMethod == "quickSort") quickStart(arr);
+      else if (sortingMethod == "mergeSort") mergeStart(arr);
+      else if (sortingMethod == "heapSort") heapStart(arr);
+      else {
+        generateArrayCheck = true;
         let selectedLanguage = document.querySelector(".language-option");
-        selectedLanguage.selectedIndex = 0; 
-        Toastify({
-          text: "Please select a Sorting Method",
-          duration: 3000,
-          gravity: "bottom",
-          position: "center"
-        }).showToast();
-        break;
+        selectedLanguage.selectedIndex = 0;
+        showToast("Please select a Sorting Method");
+      }
     }
-  }
-  else{
-    Toastify({
-      text: "Please Generate the array first",
-      duration: 3000,
-      gravity: "left",
-      position: "left"
-    }).showToast();
-  }
+    else {
+      showToast("Please Generate the array first");
+    }
   });
-
-
-
-
-
-
 });
 
-// Red - '#FF0000'
-// green - '#00FF00'
-// grey - '#AA9870'
-// violet - '#BB0123'
-  
 
 
+//controller - 4 (on clicking the reset button)
+let resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', () => { window.location.reload() });
+
+//controller - 5 (for selecting code language)
+let languageSelect = document.getElementsByClassName('language-option')[0];
+languageSelect.addEventListener('change', () => {
+  let selectedOption = document.querySelector('.options');
+  let sortingMethod = selectedOption.value;
+  if (sortingMethod == "bubbleSort") bubbleSortCode();
+  else if (sortingMethod == "selectionSort") selectionSortCode();
+  else if (sortingMethod == "insertionSort") insertionSortCode();
+  else if (sortingMethod == "quickSort") quickSortCode();
+  else if (sortingMethod == "mergeSort") mergeSortCode();
+  else if (sortingMethod == "heapSort") heapSortCode();
+  else {
+    let selectedLanguage = document.querySelector(".language-option").value;
+    showToast(`${selectedLanguage} is your prefered language.`);
+  }
+});
+
+
+//controller - 6 (for selecting sorting method)
+let sortingMethods = document.querySelector('.options');
+sortingMethods.addEventListener('change', () => {
+  let sortingMethod = document.querySelector('.options').value;
+  if (sortingMethod == "bubbleSort") bubbleSortCode();
+  else if (sortingMethod == "selectionSort") selectionSortCode();
+  else if (sortingMethod == "insertionSort") insertionSortCode();
+  else if (sortingMethod == "quickSort") quickSortCode();
+  else if (sortingMethod == "mergeSort") mergeSortCode();
+  else heapSortCode();
+});
+
+//controller - 7 (for copying code)
+let copyBtn = document.getElementById('copy-btn');
+copyBtn.addEventListener('click', () => {
+  let codeSnippet = document.getElementById('codeSnippet').textContent;
+  copyCode(codeSnippet);
+});
--- input.js
+++ input.js
@@ -1,49 +1,69 @@
-
-
-function generateRandomArray(length, min, max) {
-    let randomArray = [];
-    for (var i = 0; i < length; i++) {
-      let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-      randomArray.push(randomNumber);
-    }
-    return randomArray;
+function generateRandomArray(length) {
+  let randomArray = [];
+  let min = 1, max = 101;
+  for (var i = 0; i < length; i++) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomArray.push(randomNumber);
+  }
+  return randomArray;
 }
 
-
-
-function animateSoundBars() {
-  let soundBars = document.getElementsByClassName('innerDiv');
-  for (let i = 0; i < soundBars.length; i++) {
-    let delay = i * 10;
-    animateIndividualSoundBar(soundBars[i], delay);
+function createBars(arr) {
+  let parentDivbox2 = document.getElementById('visualBox');
+  while (parentDivbox2.firstChild) {
+    parentDivbox2.removeChild(parentDivbox2.firstChild);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let innerDiv = document.createElement('div');
+    innerDiv.style.height = `${arr[i] * 2}px`;
+    innerDiv.style.backgroundColor = '#48c486';
+    innerDiv.setAttribute('id', `elem${i}`);
+    innerDiv.classList.add("innerDiv");
+    parentDivbox2.appendChild(innerDiv);
   }
 }
 
-function animateIndividualSoundBar(soundBar, delay) {
-  let min=20,max=300;
+function animateBars() {
+  let Bars = document.getElementsByClassName('innerDiv');
+  for (let i = 0; i < Bars.length; i++) {
+    let delay = i * 10;
+    animateIndividualBar(Bars[i], delay);
+  }
+}
+
+function animateIndividualBar(Bar, delay) {
+  let min = 20, max = 300;
   setInterval(() => {
     let height = Math.floor(Math.random() * (max - min + 1)) + min; // Generate a random height for the sound bar
-    soundBar.style.height = `${height}px`;
+    Bar.style.height = `${height}px`;
   }, 50 + delay); // Adjust the interval timing as needed
 }
 
-function createBars(arr){
-  let parentDivbox2 = document.getElementById('box2');
-    while (parentDivbox2.firstChild) {
-      parentDivbox2.removeChild(parentDivbox2.firstChild);
-    }
-    for (let i = 0; i < arr.length; i++) {
-      let innerDiv = document.createElement('div');
-      innerDiv.style.height = `${arr[i] * 2}px`;
-      innerDiv.style.backgroundColor = '#00FF00';
-      innerDiv.setAttribute('id', `elem${i}`);
-      innerDiv.classList.add("innerDiv");
-      parentDivbox2.appendChild(innerDiv);
-    }
+
+function showToast(message) {
+  let toast = document.getElementById('toast');
+  toast.style.opacity = '1';
+  toast.textContent = message;
+  setTimeout(function () {
+    toast.style.opacity = '0';
+  }, 2000); // Hide the toast after 2 seconds
 }
 
-
-
-export {generateRandomArray};
-export{createBars};
-export {animateSoundBars};
+function copyCode(codeSnippet) {
+  let sortingMethod = document.querySelector('.options').value;
+  if (sortingMethod !== "Choose Sorting Method") {
+    navigator.clipboard.writeText(codeSnippet).then(function () {
+      showToast(`code copied`);
+    }, function () {
+      showToast(`Failed to copy code`);
+    });
+  }
+  else {
+    showToast(`Please select a Sorting Method`);
+  }
+}
+  export { generateRandomArray };
+  export { createBars };
+  export { animateBars };
+  export { showToast };
+  export { copyCode };
